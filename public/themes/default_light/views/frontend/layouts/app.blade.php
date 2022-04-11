4ed41de82439373950165a0e80536b71f615c927
wiledia <hello@wiledia.com>
2022-04-11 18:12:51 +0200

    fix: 🐛 light theme is using wrong base css

--- public/themes/default_light/views/frontend/layouts/app.blade.php
+++ public/themes/default_light/views/frontend/layouts/app.blade.php
@@ -37,7 +37,7 @@
     {{-- Styles --}}
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-extend.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('themes/' . Theme::getCurrent() . '/assets/css/site.css') }}?version=1.4">
+    <link rel="stylesheet" href="{{ asset('themes/default/assets/css/site.css') }}?version=1.4">
     <link rel="stylesheet" href="{{ asset('themes/' . Theme::getCurrent() . '/assets/css/site-light.css') }}?version=1.4">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/notie/notie.css') }}">
