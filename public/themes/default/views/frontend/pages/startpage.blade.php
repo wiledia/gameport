81e63f17091fa6da9cdf643f7bdc2eff05a2be09
wiledia <hello@wiledia.com>
2022-04-12 20:04:09 +0200

    test: 💍 improved startpage in light theme

--- public/themes/default/views/frontend/pages/startpage.blade.php
+++ public/themes/default/views/frontend/pages/startpage.blade.php
@@ -132,7 +132,7 @@
       <a class="col-xs-6 col-sm-4 col-md-4 col-lg-2" href="{{ $platform->url }}">
         <div class="platform">
           @if( config('settings.platform_logo') )
-            <img src="{{ asset('logos/' . $platform->acronym . '_tiny.png/') }}" alt="{{$platform->name}} Logo" @if($platform->cover_is_light)style="filter: invert(1);"@endif>
+            <img src="{{ asset('logos/' . $platform->acronym . '_tiny.png/') }}" alt="{{$platform->name}} Logo" @if($platform->cover_is_light)style="@if(Theme::getCurrent() === 'default_light')filter: unset;@else filter: invert(1);@endif"@endif>
           @else
             <span>{{ $platform->name }}</span>
           @endif
