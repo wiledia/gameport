5e011cacadc9ba7d7d9a328a7a2b0c41dcc614d2
wiledia <hello@wiledia.com>
2022-04-14 21:41:25 +0200

    fix: 🐛 platform seeder

--- database/seeds/PlatformsTableSeeder.php
+++ database/seeds/PlatformsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Platform;
 use Illuminate\Database\Seeder;
 
 class PlatformsTableSeeder extends Seeder
@@ -11,198 +12,139 @@ class PlatformsTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('platforms')->delete();
-
-        \DB::table('platforms')->insert([
-            0 => [
-                'id' => 1,
+        Platform::updateOrCreate([
+            [
                 'name' => 'PC',
-                'description' => null,
                 'color' => '#000000',
                 'acronym' => 'pc',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => '2017-01-15 12:40:29',
             ],
-            1 => [
-                'id' => 2,
+            [
                 'name' => 'PlayStation 4',
                 'description' => 'Sony',
                 'color' => '#003791',
                 'acronym' => 'ps4',
                 'cover_position' => 'left',
-                'created_at' => '2016-11-30 20:58:21',
-                'updated_at' => '2017-01-22 14:19:14',
             ],
-            2 => [
-                'id' => 3,
+            [
                 'name' => 'Xbox One',
                 'description' => 'Microsoft',
                 'color' => '#107c10',
                 'acronym' => 'xboxone',
                 'cover_position' => 'center',
-                'created_at' => '2016-11-30 20:58:52',
-                'updated_at' => '2017-01-11 10:48:02',
             ],
-            3 => [
-                'id' => 4,
+            [
                 'name' => 'Nintendo Switch',
                 'description' => 'Nintendo',
                 'color' => '#e60012',
                 'acronym' => 'switch',
                 'cover_position' => 'left',
-                'created_at' => '2017-01-22 14:05:09',
-                'updated_at' => '2017-01-22 14:05:09',
             ],
-            4 => [
+            [
                 'id' => 5,
                 'name' => 'Wii U',
                 'description' => 'Nintendo',
                 'color' => '#009ac7',
                 'acronym' => 'wii-u',
                 'cover_position' => 'center',
-                'created_at' => null,
-                'updated_at' => '2017-01-11 10:47:57',
             ],
-            5 => [
-                'id' => 6,
+            [
                 'name' => 'PlayStation 3',
                 'description' => 'Sony',
                 'color' => '#326db3',
                 'acronym' => 'ps3',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            6 => [
-                'id' => 7,
+            [
                 'name' => 'Xbox 360',
                 'description' => 'Microsoft',
                 'color' => '#a4c955',
                 'acronym' => 'xbox360',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            7 => [
-                'id' => 8,
+            [
                 'name' => 'Nintendo 3DS',
                 'description' => 'Nintendo',
                 'color' => '#c90f17',
                 'acronym' => '3ds',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            8 => [
-                'id' => 9,
+            [
                 'name' => 'PlayStation Vita',
                 'description' => 'Sony',
                 'color' => '#1654bd',
                 'acronym' => 'vita',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            9 => [
-                'id' => 10,
+            [
                 'name' => 'Nintendo DS',
                 'description' => 'Nintendo',
                 'color' => '#929497',
                 'acronym' => 'ds',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            10 => [
-                'id' => 11,
+            [
                 'name' => 'PlayStation 2',
                 'description' => 'Sony',
                 'color' => '#140c7a',
                 'acronym' => 'ps2',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            11 => [
-                'id' => 12,
+            [
                 'name' => 'Xbox',
                 'description' => 'Microsoft',
                 'color' => '#93c83e',
                 'acronym' => 'xbox',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            12 => [
-                'id' => 13,
+            [
                 'name' => 'PlayStation',
                 'description' => 'Sony',
                 'color' => '#4081bc',
                 'acronym' => 'ps',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            13 => [
-                'id' => 14,
+            [
                 'name' => 'Wii',
                 'description' => 'Nintendo',
                 'color' => '#32ccfe',
                 'acronym' => 'wii',
                 'cover_position' => 'right',
-                'created_at' => null,
-                'updated_at' => '2017-01-11 10:48:51',
             ],
-            14 => [
-                'id' => 15,
+            [
                 'name' => 'Gamecube',
                 'description' => 'Nintendo',
                 'color' => '#663399',
                 'acronym' => 'gamecube',
                 'cover_position' => 'center',
-                'created_at' => null,
-                'updated_at' => '2017-01-11 10:49:10',
             ],
-            15 => [
-                'id' => 16,
+            [
                 'name' => 'Nintendo 64',
                 'description' => 'Nintendo',
                 'color' => '#fdbf2d',
                 'acronym' => 'n64',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            16 => [
-                'id' => 17,
+            [
                 'name' => 'Game Boy Advance',
                 'description' => 'Nintendo',
                 'color' => '#1f00cc',
                 'acronym' => 'gba',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            17 => [
-                'id' => 18,
+            [
                 'name' => 'PlayStation Portable',
                 'description' => 'Sony',
                 'color' => '#8e92af',
                 'acronym' => 'psp',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
-            18 => [
-                'id' => 19,
+            [
                 'name' => 'Dreamcast',
                 'description' => 'Sega',
                 'color' => '#4365a2',
                 'acronym' => 'dreamcast',
                 'cover_position' => 'left',
-                'created_at' => null,
-                'updated_at' => null,
             ],
         ]);
     }
