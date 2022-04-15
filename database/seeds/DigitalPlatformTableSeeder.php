50f5c2885f1986e09a59611a2e1d1c8bc755b888
wiledia <hello@wiledia.com>
2022-04-15 19:58:27 +0200

    fix: 🐛 table seeder namespaces

--- database/seeds/DigitalPlatformTableSeeder.php
+++ database/seeds/DigitalPlatformTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class DigitalPlatformTableSeeder extends Seeder
--- database/seeds/PlatformsTableSeeder.php
+++ database/seeds/PlatformsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Platform;
 use Illuminate\Database\Seeder;
 
