9bbb53a45b0790b93dd500ea564adc2083350a27
wiledia <hello@wiledia.com>
2022-06-26 18:52:07 +0200

    feat: 🎸 direct to https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\HttpsMiddleware::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LogLastUserActivity::class,
             \App\Http\Middleware\LocaleMiddleware::class,
